b30cdc717e93d3f1fbc5d859c2bf61bce35eb475
K.Kirubakaran <k.kirubakaran@stackroute.in>
2023-02-20 19:52:16 +0530

    first commit

---
+++ src/main/java/org/example/Helloworld.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Helloworld {//class definition
+    /*
+    multiple line comments
+     */
+    /**documentation*/
+
+    public static void main(String[] args) {
+        //statements to be executed
+        System.out.println("Hello sri");
+    }
+}
